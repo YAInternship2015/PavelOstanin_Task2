90edc32ab9f6d56e86ba5d177c17209dd699e011
Pavel Ostanin <pavel.os@kttsoft.com>
2015-08-13 09:39:56 +0300

    Initial Commit

---
+++ TaskTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskTableView
+//
+//  Created by Pavel Ostanin on 13/08/2015.
+//  Copyright (c) 2015 Pavel Ostanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaskTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaskTableView
+//
+//  Created by Pavel Ostanin on 13/08/2015.
+//  Copyright (c) 2015 Pavel Ostanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
